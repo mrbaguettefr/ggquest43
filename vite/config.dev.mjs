4d52e9fd9df726fe7b19df0471ea9b8eee097529
tony <tony.lbvre@gmail.com>
2026-05-04 12:11:33 +0200

    upg

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -5,8 +5,10 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                manualChunks: {
-                    phaser: ['phaser']
+                manualChunks(id) {
+                    if (id.includes('/node_modules/phaser/')) {
+                        return 'phaser';
+                    }
                 }
             }
         },
